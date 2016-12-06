0989485e7c20d850d6d06c6abba6202537f69a50
Nikita <tyumencev2004@bk.ru>
2016-12-06 16:22:08 +0300

    [CREATE] We need fire extinguishers!

---
+++ WTFMotherFucker/body.js
@@ -0,0 +1,47 @@
+﻿var duration = 0.03
+var castTime = 0.0
+var interval = duration - castTime
+//-------------------
+
+function WTFMotherFuckerOnInterval() {
+	var MyEnt = Players.GetPlayerHeroEntityIndex(Game.GetLocalPlayerID())
+	if(Entities.IsStunned(MyEnt) || !Entities.IsAlive(MyEnt))
+		return
+	var HEnts = Entities.GetAllHeroEntities()
+	
+	ExecNoTarget(MyEnt, "batrider_firefly")
+	ExecNoTarget(MyEnt, "wisp_spirits")
+}
+
+function ExecNoTarget(MyEnt, AbilName) {
+	var Abil = Game.GetAbilityByName(MyEnt, AbilName)
+	if(!Abilities.IsDisplayedAbility(Abil))
+		return
+	var AbilLvl = parseInt(Abilities.GetLevel(Abil))
+	if(AbilLvl === 0)
+		return
+	
+	//Game.EntStop(MyEnt)
+	Game.CastNoTarget(MyEnt, Abil, false)
+}
+
+function WTFMotherFuckerOnToggle() {
+	if (!WTFMotherFucker.checked)
+		Game.ScriptLogMsg('Script disabled: WTFMotherFucker', '#ff0000')
+	else {
+		function intervalFunc(){
+			$.Schedule(
+				interval,
+				function() {
+					WTFMotherFuckerOnInterval()
+					if(WTFMotherFucker.checked)
+						intervalFunc()
+				}
+			)
+		}
+		intervalFunc()
+		Game.ScriptLogMsg('Script enabled: WTFMotherFucker', '#00ff00')
+	}
+}
+
+var WTFMotherFucker = Game.AddScript('WTFMotherFucker', WTFMotherFuckerOnToggle)
