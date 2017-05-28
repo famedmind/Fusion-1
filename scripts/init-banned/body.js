882433989fead24125390c68acfd7b02c05998aa
Nikita <MoofMonkey@users.noreply.github.com>
2017-05-28 17:53:41 +0300

    t

--- scripts/init-banned/body.js
+++ scripts/init-banned/body.js
@@ -1,7 +1,15 @@
-D2JSEvents.SendEventClientSide (
-	'antiaddiction_toast',
-	{
-		"message": "You're permanently banned from this D2JS server.",
-		"duration": "999999"
+GameEvents.Subscribe('game_newmap', function(data) {
+	function f() {
+		if(Players.GetLocalPlayer() !== -1)
+			GameEvents.SendEventClientSide (
+				'antiaddiction_toast',
+				{
+					"message": "You're permanently banned from this D2JS server.",
+					"duration": "999999"
+				}
+			)
+		else
+			$.Schedule(1, f)
 	}
-)
+	f()
+}
