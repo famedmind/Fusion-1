255683b99784e27e0eeace3c323242b74e35e598
Nikita <MoofMonkey@users.noreply.github.com>
2017-06-27 14:25:14 +0300

    Added some more debug

--- scripts/init/body.js
+++ scripts/init/body.js
@@ -46,7 +46,7 @@ Fusion.ServerRequest = function(name, val, callback) {
 			if (a.status === 200 && a.responseText !== null)
 				callback(a.responseText.substring(0, a.responseText.length - 3))
 			else
-				$.Msg("Can't load \"" + name + "\" @ " + val)
+				$.Msg("Can't load \"" + name + "\" @ " + val + ", returned " + JSON.stringify(a))
 		}
 	}
 	args['data'][name] = val
